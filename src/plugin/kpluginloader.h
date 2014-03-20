628533cbfa602f90146dc63b835237e001431da2
Alex Merry <alex.merry@kde.org>
2014-03-20 22:09:42 +0000

    Improve the KPluginLoader documentation
    (REVIEW): 116938
    
    Commit 4a3a5699f4d5b81bc59cb9de403f5d292a08f561 in kservice

--- src/plugin/kpluginloader.h
+++ src/plugin/kpluginloader.h
@@ -31,28 +31,39 @@ class KPluginLoaderPrivate;
 /**
  * \class KPluginLoader kpluginloader.h <KPluginLoader>
  *
- * This class can be used to dynamically load a plugin library at runtime.
+ * This class extends QPluginLoader in three ways:
  *
- * This class makes sure that the Qt and KDE versions used to compile this library aren't newer than
- * the ones currently used.
+ * - it additionally searches for plugins in the "kf5" subdirectories of
+ *   directories in QCoreApplication::libraryPaths() (see findPlugin()); this
+ *   corresponds to the ${PLUGIN_INSTALL_DIR} of the KDEInstallDirs CMake module
+ *   in extra-cmake-modules
+ * - it reads the plugin version, as provided by the K_EXPORT_PLUGIN_VERSION macro
+ *   (see pluginVersion())
+ * - it provides access to a KPluginFactory instance if the plugin provides one
+ *   (see factory())
  *
+ * Unlike QPluginLoader, KPluginLoader does not allow re-use by changing the
+ * fileName property.
  *
- * This class is reentrant, you can load plugins from different threads. You can also have multiple
- * PluginLoaders for one library without negative effects.
- * The object obtained with factory() or the inherited method QPluginLoader::instance() is
- * cached inside the library. If you call factory() or instance() multiple times, you will always get
- * the same object, even from different threads and different KPluginLoader instances.
- * You can delete this object easily, a new one will be created if factory() or instance() is called
+ * This class is reentrant, you can load plugins from different threads. You can
+ * also have multiple PluginLoaders for one library without negative effects.
+ * The object obtained with factory() or the inherited method
+ * QPluginLoader::instance() is cached inside the library. If you call factory()
+ * or instance() multiple times, you will always get the same object, even from
+ * different threads and different KPluginLoader instances.  You can delete this
+ * object easily, a new one will be created if factory() or instance() is called
  * afterwards. factory() uses instance() internally.
  *
- * KPluginLoader inherits QPluginLoader::unload(). It is safe to call this method if you loaded a plugin
- * and decide not to use it for some reason. But as soon as you start to use the factory from the plugin,
- * you should stay away from it. It's nearly impossible to keep track of all objects created directly or
- * indirectly from the plugin and all other pointers into plugin code. Using unload() in this case is asking
- * for trouble. If you really need to unload your plugins, you have to take care to convert the clipboard
- * content to text, because the plugin could have registered a custom mime source. You also have to delete
- * the factory of the plugin, otherwise you will create a leak.
- * The destructor of KPluginLoader doesn't call unload.
+ * KPluginLoader inherits QPluginLoader::unload(). It is safe to call this
+ * method if you loaded a plugin and decide not to use it for some reason. But
+ * as soon as you start to use the factory from the plugin, you should stay away
+ * from it. It's nearly impossible to keep track of all objects created directly
+ * or indirectly from the plugin and all other pointers into plugin code. Using
+ * unload() in this case is asking for trouble. If you really need to unload
+ * your plugins, you have to take care to convert the clipboard content to text,
+ * because the plugin could have registered a custom mime source. You also have
+ * to delete the factory of the plugin, otherwise you will create a leak.  The
+ * destructor of KPluginLoader doesn't call unload.
  *
  * Sample code:
  * \code
@@ -77,52 +88,70 @@ class KSERVICE_EXPORT KPluginLoader : public QPluginLoader
     Q_OBJECT
 public:
     /**
-     * Used this constructor to load a plugin with a given library name. Plugin libraries shouldn't have a 'lib' prefix.
+     * Load a plugin by name.
+     *
+     * This should be the name of the plugin object file, without any suffix
+     * (like .so or .dll).  Plugin object files should not have a 'lib' prefix.
      *
      * errorString() will be set if problems are encountered.
      *
-     * \param plugin The name of the plugin library.
+     * \param plugin The name of the plugin.
      * \param parent A parent object.
      */
     explicit KPluginLoader(const QString &plugin, QObject *parent = 0);
 
     /**
-     * Used this constructor to load a plugin from a service. The service must contain a library.
+     * load a plugin from a service.
+     *
+     * The service must contain a library.
      *
      * errorString() will be set if problems are encountered.
      *
-     * \param service The service for which the library should be loaded.
+     * \param service The service that provides the plugin.
      * \param parent A parent object.
      */
     explicit KPluginLoader(const KService &service, QObject *parent = 0);
 
     /**
      * Destroys the plugin loader.
+     *
+     * Unless unload() was called explicitly, the plugin stays in memory until
+     * the application terminates.
      */
     ~KPluginLoader();
 
     /**
-     * Used to obtain the factory object of the plugin. The returned object is usually created
-     * from a K_PLUGIN_FACTORY invocation.
+     * Returns the factory object of the plugin.
+     *
+     * This is typically created by one of the KPluginFactory macros.
+     * Internally, this uses QPluginLoader::instance(), and the same
+     * behaviours apply.
      *
      * \returns The factory of the plugin or 0 on error.
      */
     KPluginFactory *factory();
 
     /**
-     * The name of this plugin as given to the constructor.
-     * \returns the plugin name
+     * Returns the name of this plugin as given to the constructor.
+     *
+     * If the KService constructor was used, this is the name of the library
+     * provided by the service.
+     *
+     * \returns The plugin name.
      */
     QString pluginName() const;
 
     /**
-     * Queries the plugin version.
-     * \returns The version given to K_EXPORT_PLUGIN_VERSION or (quint32) -1 if not set.
+     * Returns the plugin version.
+     *
+     * \returns The version given to K_EXPORT_PLUGIN_VERSION, or (quint32) -1 if
+     *          the macro was not used.
      */
     quint32 pluginVersion() const;
 
     /**
      * Queries the last error.
+     *
      * \returns The description of the last error.
      */
     QString errorString() const;
