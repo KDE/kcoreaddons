KDE/kcoreaddons

commit 9ae6d765b37135bbfe3a8b936e5a88b8a435e424

Fix some statically detected issues
Make sure attributes are initialized on construction.
Properly convert translated strings
Define as unreachable code that isn't.

REVIEW: 127209

src/lib/randomness/krandom.cpp
@@ -46,10 +46,10 @@ int KRandom::random()
         bool opened = urandom.open(QIODevice::ReadOnly | QIODevice::Unbuffered);
         if (!opened || urandom.read(reinterpret_cast<char *>(&seed), sizeof(seed)) != sizeof(seed)) {
             // No /dev/urandom... try something else.
-            srand(getpid());
-            seed = rand() + time(0);
+            qsrand(getpid());
+            seed = qrand() + time(0);
         }
-        srand(seed);
+        qsrand(seed);
     }
     return rand();
 }

src/lib/io/kdirwatch.cpp
@@ -177,7 +177,11 @@ KDirWatchPrivate::KDirWatchPrivate()
       statEntries(0),
       delayRemove(false),
       rescan_all(false),
-      rescan_timer()
+      rescan_timer(),
+#if HAVE_SYS_INOTIFY_H
+      mSn(Q_NULLPTR),
+#endif
+      _isStopped(false)
 {
     timer.setObjectName(QStringLiteral("KDirWatchPrivate::timer"));
     connect(&timer, SIGNAL(timeout()), this, SLOT(slotRescan()));
@@ -1880,8 +1884,6 @@ KDirWatch::KDirWatch(QObject *parent)
     static QBasicAtomicInt nameCounter = Q_BASIC_ATOMIC_INITIALIZER(1);
     const int counter = nameCounter.fetchAndAddRelaxed(1); // returns the old value
     setObjectName(QStringLiteral("KDirWatch-%1").arg(counter));
-
-    d->_isStopped = false;
 
     if (counter == 1) { // very first KDirWatch instance
         // Must delete QFileSystemWatcher before qApp is gone - bug 261541

src/lib/io/kprocess.cpp
@@ -37,9 +37,8 @@
 
 KProcess::KProcess(QObject *parent) :
     QProcess(parent),
-    d_ptr(new KProcessPrivate)
+    d_ptr(new KProcessPrivate(this))
 {
-    d_ptr->q_ptr = this;
     setOutputChannelMode(ForwardedChannels);
 }
 

src/lib/io/kprocess_p.h
@@ -28,8 +28,9 @@ class KProcessPrivate
 {
     Q_DECLARE_PUBLIC(KProcess)
 protected:
-    KProcessPrivate() :
-        openMode(QIODevice::ReadWrite)
+    KProcessPrivate(KProcess* q) :
+        openMode(QIODevice::ReadWrite),
+        q_ptr(q)
     {
     }
 

src/lib/kaboutdata.cpp
@@ -146,6 +146,7 @@ KAboutLicense::Private::Private(LicenseKey licenseType, const KAboutData *aboutD
 
 KAboutLicense::Private::Private(const KAboutData *aboutData)
     : QSharedData(),
+      _licenseKey(Unknown),
       _aboutData(aboutData)
 {
 }
@@ -989,7 +990,7 @@ void KAboutData::processCommandLine(QCommandLineParser *parser)
         if (d->_authorList.isEmpty()) {
             printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "This application was written by somebody who wants to remain anonymous.")));
         } else {
-            printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "%s was written by:")), qPrintable(qAppName()));
+            printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "%1 was written by:").arg(qAppName())));
             Q_FOREACH (const KAboutPerson &person, d->_authorList) {
                 QString authorData = QStringLiteral("    ") + person.name();
                 if (!person.emailAddress().isEmpty()) {
@@ -1002,7 +1003,7 @@ void KAboutData::processCommandLine(QCommandLineParser *parser)
             if (bugAddress() == QLatin1String("submit@bugs.kde.org") ) {
                 printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "Please use http://bugs.kde.org to report bugs.")));
             } else if (!bugAddress().isEmpty()) {
-                printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "Please report bugs to %s.")), qPrintable(bugAddress()));
+                printf("%s\n", qPrintable(QCoreApplication::translate("KAboutData CLI", "Please report bugs to %1.").arg(bugAddress())));
             }
         } else {
             printf("%s\n", qPrintable(customAuthorPlainText()));

src/lib/util/kformatprivate.cpp
@@ -257,8 +257,7 @@ QString KFormatPrivate::formatDuration(quint64 msecs, KFormat::DurationFormatOpt
 
     }
 
-    // Should never reach here
-    Q_ASSERT(false);
+    Q_UNREACHABLE();
     return QString();
 }
 
@@ -363,7 +362,7 @@ QString KFormatPrivate::formatSpelloutDuration(quint64 msecs) const
 
 QString KFormatPrivate::formatRelativeDate(const QDate &date, QLocale::FormatType format) const
 {
-    int daysTo = QDate::currentDate().daysTo(date);
+    const int daysTo = QDate::currentDate().daysTo(date);
     if (daysTo > 7 || daysTo < -7) {
         return m_locale.toString(date, format);
     }
@@ -388,8 +387,7 @@ QString KFormatPrivate::formatRelativeDate(const QDate &date, QLocale::FormatTyp
     {
         return tr("Next %1").arg(m_locale.dayName(date.dayOfWeek(), format));
     }
-
-    return m_locale.toString(date, format);
+    Q_UNREACHABLE();
 }
 
 QString KFormatPrivate::formatRelativeDateTime(const QDateTime &dateTime, QLocale::FormatType format) const

commit 78212436643af95779facd9593c82fb149c2213d

Missing rand() -> qrand
Fixes regression introduced in 9ae6d765b37135bbfe3a8b936e5a88b8a435e424

Reviewed by Aleix

BUGS: 362161

src/lib/randomness/krandom.cpp
@@ -51,7 +51,7 @@ int KRandom::random()
         }
         qsrand(seed);
     }
-    return rand();
+    return qrand();
 }
 
 QString KRandom::randomString(int length)