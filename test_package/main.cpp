57115675422138dca49b39e79e298abc4d9df9b6
Ovidiu-Florin Bogdan <ovidiu-florin.bogdan@intel.com>
2018-08-12 00:59:49 +0300

    add a conan package test

---
+++ test_package/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include <QString>
+
+#include <KCoreAddons>
+
+static const QString TESTVALUE="5.50.0";
+
+int main() {
+
+    QString readValue = KCoreAddons::versionString();
+
+    if (readValue == TESTVALUE) {
+        std::cout << "Test OK" << std::endl;
+        return 0;
+    }
+    std::cout << "Test FAILED" << std::endl;
+    return 1;
+}
