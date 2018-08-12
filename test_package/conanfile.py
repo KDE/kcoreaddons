aae06fb22b0ec8779696cff1d35470872a7434a7
Ovidiu-Florin Bogdan <ovidiu-florin.bogdan@intel.com>
2018-08-12 17:05:35 +0300

    Rename conan test class

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,7 +3,7 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class KconfigTestConan(ConanFile):
+class KCoreAddonsTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
