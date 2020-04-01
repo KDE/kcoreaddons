KDE/kcoreaddons

commit fcdf263339b4493432e14dcca77e59b5dccd5531

Fix Warnings
Summary:
We had 10 compile warnings before.
0 after

Reviewers: #frameworks, davidedmundson

Reviewed By: davidedmundson

Subscribers: davidedmundson, kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D28349

src/lib/io/kdirwatch.cpp
@@ -952,6 +952,8 @@ void KDirWatchPrivate::addWatch(Entry *e)
     switch (preferredMethod) {
 #if HAVE_FAM
     case KDirWatch::FAM: entryAdded = useFAM(e); break;
+#else
+    case KDirWatch::FAM: Q_UNREACHABLE(); break;
 #endif
 #if HAVE_SYS_INOTIFY_H
     case KDirWatch::INotify: entryAdded = useINotify(e); break;

autotests/kprocesstest_helper.cpp
@@ -14,6 +14,10 @@
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("Missing parameter");
+        return -1;
+    }
     KProcess p;
     p.setShellCommand(QString::fromLatin1("echo " EOUT "; echo " EERR " >&2"));
     p.setOutputChannelMode(static_cast<KProcess::OutputChannelMode>(atoi(argv[1])));

src/lib/caching/kshareddatacache.cpp
@@ -78,7 +78,9 @@ static unsigned int MurmurHashAligned(const void *key, int len, unsigned int see
 
         switch (align) {
         case 1: t |= data[2] << 16;
+            Q_FALLTHROUGH();
         case 2: t |= data[1] << 8;
+            Q_FALLTHROUGH();
         case 3: t |= data[0];
         }
 
@@ -112,8 +114,11 @@ static unsigned int MurmurHashAligned(const void *key, int len, unsigned int see
 
         switch (pack) {
         case 3: d |= data[2] << 16;
+            Q_FALLTHROUGH();
         case 2: d |= data[1] << 8;
+            Q_FALLTHROUGH();
         case 1: d |= data[0];
+            Q_FALLTHROUGH();
         case 0: h += (t >> sr) | (d << sl);
             h *= m;
             h ^= h >> r;
@@ -137,7 +142,9 @@ static unsigned int MurmurHashAligned(const void *key, int len, unsigned int see
 
     switch (len) {
     case 3: h += data[2] << 16;
+        Q_FALLTHROUGH();
     case 2: h += data[1] << 8;
+        Q_FALLTHROUGH();
     case 1: h += data[0];
         h *= m;
         h ^= h >> r;

commit 0ee65124c0f6fc436c55ba1fca2371edd85a4777

kdirwatch: fix a recently introduced crash
Summary:
Prevent a crash when browsing nfs without FAM installed introduced in D28349

BUG: 419428

Reviewers: bruns, #frameworks, iasensio

Reviewed By: bruns, iasensio

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D28457

src/lib/io/kdirwatch.cpp
@@ -953,13 +953,17 @@ void KDirWatchPrivate::addWatch(Entry *e)
 #if HAVE_FAM
     case KDirWatch::FAM: entryAdded = useFAM(e); break;
 #else
-    case KDirWatch::FAM: Q_UNREACHABLE(); break;
+    case KDirWatch::FAM: entryAdded = false; break;
 #endif
 #if HAVE_SYS_INOTIFY_H
     case KDirWatch::INotify: entryAdded = useINotify(e); break;
+#else
+    case KDirWatch::INotify: entryAdded = false; break;
 #endif
 #if HAVE_QFILESYSTEMWATCHER
     case KDirWatch::QFSWatch: entryAdded = useQFSWatch(e); break;
+#else
+    case KDirWatch::QFSWatch: entryAdded = false; break;
 #endif
     case KDirWatch::Stat: entryAdded = useStat(e); break;
     }