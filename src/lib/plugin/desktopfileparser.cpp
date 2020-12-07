d08b3ed6b7abae6299a5b36638e66482b07fd28a
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-07 14:29:09 +0100

    desktop to JSON conversion: ignore "Actions=" entry

--- src/lib/plugin/desktopfileparser.cpp
+++ src/lib/plugin/desktopfileparser.cpp
@@ -540,10 +540,15 @@ void DesktopFileParser::convertToJson(const QByteArray &key, ServiceTypeDefiniti
         DESKTOPTOJSON_VERBOSE_WARNING << "Hidden= key found in desktop file, this makes no sense"
                 " with metadata inside the plugin.";
         kplugin[QString::fromUtf8(key)] = (value.toLower() == QLatin1String("true"));
-    } else if (key == QByteArrayLiteral("Exec") || key == QByteArrayLiteral("Type")
-            || key == QByteArrayLiteral("X-KDE-Library") || key == QByteArrayLiteral("Encoding")) {
+    } else if (key == QByteArrayLiteral("Exec") ||
+               key == QByteArrayLiteral("Type") ||
+               key == QByteArrayLiteral("Actions") ||
+               key == QByteArrayLiteral("X-KDE-Library") ||
+               key == QByteArrayLiteral("Encoding")) {
         // Exec= doesn't make sense here, however some .desktop files (like e.g. in kdevelop) have a dummy value here
         // also the Type=Service entry is no longer needed
+        // Actions= is used as hack at least with the Dolphin KPart to report the different view mode options
+        // no plans yet to port that hack to JSON, so gently ignore it for now
         // X-KDE-Library is also not needed since we already have the library to read this metadata
         // Encoding= is also not converted as we always use utf-8 for reading
         DESKTOPTOJSON_VERBOSE_DEBUG << "Not converting key " << key << "=" << value;
