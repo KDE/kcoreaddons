8d76bc19b60a7dcd004be904ab7851807b69932e
Waqar Ahmed <waqar.17a@gmail.com>
2021-01-21 22:44:42 +0500

    Add KFuzzyMatcher for fuzzy filtering of strings
    This is a proposal to add a fuzzy matching utility to KCoreAddons. I added
    it to Kate initially to test its capabilities and it has been amazing so
    far.
    
    This utility does fuzzy matching and outputs two results i.e., a boolean
    and an integer (success, score). The score is calculated on the basis of
    distance, whether the match was found on separators or was it
    sequential. This makes it useful for a variety of applications but with
    some tuning for usecases might be necessary in order to produce better
    results.
    
    The original author wrote a blog post about it which can be read for
    more detailed info on this:
    https://www.forrestthewoods.com/blog/reverse_engineering_sublime_texts_fuzzy_match

---
+++ src/lib/text/kfuzzymatcher.cpp
@@ -0,0 +1,217 @@
+/*
+    This file is part of the KDE libraries
+
+    SPDX-FileCopyrightText: 2017 Forrest Smith <forrestthewoods@gmail.com>
+    SPDX-FileCopyrightText: 2021 Waqar Ahmed   <waqar.17a@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "kfuzzymatcher.h"
+
+#include <QString>
+#include <QStringView>
+
+// internal
+// clang-format off
+static bool match_recursive(QStringView::const_iterator pattern,
+                            QStringView::const_iterator str,
+                            int &outScore,
+                            const QStringView::const_iterator strBegin,
+                            const QStringView::const_iterator strEnd,
+                            const QStringView::const_iterator patternEnd,
+                            const uint8_t *srcMatches,
+                            uint8_t *matches,
+                            int maxMatches,
+                            int nextMatch,
+                            int &recursionCount,
+                            int seqBonus = 15)
+{
+    static constexpr int recursionLimit = 10;
+    // Count recursions
+    ++recursionCount;
+    if (recursionCount >= recursionLimit)
+        return false;
+
+    // Detect end of strings
+    if (pattern == patternEnd || str == strEnd)
+        return false;
+
+    // Recursion params
+    bool recursiveMatch = false;
+    uint8_t bestRecursiveMatches[256];
+    int bestRecursiveScore = 0;
+
+    // Loop through pattern and str looking for a match
+    bool firstMatch = true;
+    while (pattern != patternEnd && str != strEnd) {
+        // Found match
+        if (pattern->toLower() == str->toLower()) {
+            // Supplied matches buffer was too short
+            if (nextMatch >= maxMatches)
+                return false;
+
+            // "Copy-on-Write" srcMatches into matches
+            if (firstMatch && srcMatches) {
+                memcpy(matches, srcMatches, nextMatch);
+                firstMatch = false;
+            }
+
+            // Recursive call that "skips" this match
+            uint8_t recursiveMatches[256];
+            int recursiveScore;
+            auto strNextChar = std::next(str);
+            if (match_recursive(pattern, strNextChar, recursiveScore, strBegin,
+                                strEnd, patternEnd, matches, recursiveMatches,
+                                sizeof(recursiveMatches), nextMatch, recursionCount)) {
+                // Pick best recursive score
+                if (!recursiveMatch || recursiveScore > bestRecursiveScore) {
+                    memcpy(bestRecursiveMatches, recursiveMatches, 256);
+                    bestRecursiveScore = recursiveScore;
+                }
+                recursiveMatch = true;
+            }
+
+            // Advance
+            matches[nextMatch++] = (uint8_t)(std::distance(strBegin, str));
+            ++pattern;
+        }
+        ++str;
+    }
+
+    // Determine if full pattern was matched
+    bool matched = pattern == patternEnd ? true : false;
+
+    // Calculate score
+    if (matched) {
+        int sequentialBonus = seqBonus; // bonus for adjacent matches
+        static constexpr int separatorBonus = 30; // bonus if match occurs after a separator
+        static constexpr int camelBonus = 30; // bonus if match is uppercase and prev is lower
+        static constexpr int firstLetterBonus = 15; // bonus if the first letter is matched
+
+        static constexpr int leadingLetterPenalty = -5; // penalty applied for every letter in str before the first match
+        static constexpr int maxLeadingLetterPenalty = -15; // maximum penalty for leading letters
+        static constexpr int unmatchedLetterPenalty = -1; // penalty for every letter that doesn't matter
+
+        // Iterate str to end
+        while (str != strEnd)
+            ++str;
+
+        // Initialize score
+        outScore = 100;
+
+        // Apply leading letter penalty
+        int penalty = leadingLetterPenalty * matches[0];
+        if (penalty < maxLeadingLetterPenalty)
+            penalty = maxLeadingLetterPenalty;
+        outScore += penalty;
+
+        // Apply unmatched penalty
+        const int unmatched = (int)(std::distance(strBegin, str)) - nextMatch;
+        outScore += unmatchedLetterPenalty * unmatched;
+
+        // Apply ordering bonuses
+        for (int i = 0; i < nextMatch; ++i) {
+            uint8_t currIdx = matches[i];
+
+            if (i > 0) {
+                uint8_t prevIdx = matches[i - 1];
+
+                // Sequential
+                if (currIdx == (prevIdx + 1))
+                    outScore += sequentialBonus;
+            }
+
+            // Check for bonuses based on neighbor character value
+            if (currIdx > 0) {
+                // Camel case
+                QChar neighbor = *(strBegin + currIdx - 1);
+                QChar curr = *(strBegin + currIdx);
+                if (neighbor.isLower() && curr.isUpper())
+                    outScore += camelBonus;
+
+                // Separator
+                bool neighborSeparator = neighbor == QLatin1Char('_') || neighbor == QLatin1Char(' ');
+                if (neighborSeparator)
+                    outScore += separatorBonus;
+            } else {
+                // First letter
+                outScore += firstLetterBonus;
+            }
+        }
+    }
+
+    // Return best result
+    if (recursiveMatch && (!matched || bestRecursiveScore > outScore)) {
+        // Recursive score is better than "this"
+        memcpy(matches, bestRecursiveMatches, maxMatches);
+        outScore = bestRecursiveScore;
+        return true;
+    } else if (matched) {
+        // "this" score is better than recursive
+        return true;
+    } else {
+        // no match
+        return false;
+    }
+}
+// clang-format on
+
+static bool match_internal(const QStringView pattern, const QStringView str, int &outScore, unsigned char *matches, int maxMatches)
+{
+    int recursionCount = 0;
+
+    auto strIt = str.cbegin();
+    auto patternIt = pattern.cbegin();
+    const auto patternEnd = pattern.cend();
+    const auto strEnd = str.cend();
+
+    return match_recursive(patternIt, strIt, outScore, strIt, strEnd, patternEnd,
+                           nullptr, matches, maxMatches, 0, recursionCount);
+}
+
+/**************************************************************/
+
+QString KFuzzyMatcher::toFuzzyMatchedDisplayString(const QStringView pattern, QString &str, const QString &htmlTag, const QString &htmlTagClose)
+{
+    // TODO: improve this so that we don't have to put html tags on every char
+    // probably using some kind of intervals container
+    int j = 0;
+    for (int i = 0; i < str.size() && j < pattern.size(); ++i) {
+        if (str.at(i).toLower() == pattern.at(j).toLower()) {
+            str.replace(i, 1, htmlTag + str.at(i) + htmlTagClose);
+            i += htmlTag.size() + htmlTagClose.size();
+            ++j;
+        }
+    }
+    return str;
+}
+
+bool KFuzzyMatcher::matchSimple(const QStringView pattern, const QStringView str)
+{
+    auto patternIt = pattern.cbegin();
+    for (auto strIt = str.cbegin(); strIt != str.cend() && patternIt != pattern.cend(); ++strIt) {
+        if (strIt->toLower() == patternIt->toLower())
+            ++patternIt;
+    }
+    return patternIt == pattern.cend();
+}
+
+bool KFuzzyMatcher::matchSequential(const QStringView pattern, const QStringView str, int &outScore)
+{
+    int recursionCount = 0;
+    uint8_t matches[256];
+    auto maxMatches = sizeof(matches);
+    auto strIt = str.cbegin();
+    auto patternIt = pattern.cbegin();
+    const auto patternEnd = pattern.cend();
+    const auto strEnd = str.cend();
+
+    return match_recursive(patternIt, strIt, outScore, strIt, strEnd, patternEnd,
+                           nullptr, matches, maxMatches, 0, recursionCount, 40);
+}
+
+bool KFuzzyMatcher::match(const QStringView pattern, const QStringView str, int &outScore)
+{
+    uint8_t matches[256];
+    return match_internal(pattern, str, outScore, matches, sizeof(matches));
+}
---
+++ src/lib/text/kfuzzymatcher.h
@@ -0,0 +1,108 @@
+/*
+    This file is part of the KDE libraries
+
+    SPDX-FileCopyrightText: 2017 Forrest Smith <forrestthewoods@gmail.com>
+    SPDX-FileCopyrightText: 2021 Waqar Ahmed <waqar.17a@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include <kcoreaddons_export.h>
+
+class QString;
+class QStringView;
+
+/**
+ * This namespace contains functions for fuzzy matching of strings.
+ *
+ * This code is ported to Qt from
+ * lib_fts https://github.com/forrestthewoods/lib_fts which tries to
+ * replicate SublimeText like fuzzy matching
+ *
+ * All methods in here except toFuzzyMatchedDisplayString() are stateless.
+ *
+ * @short Namespace for fuzzy matching of strings
+ * @author Waqar Ahmed <waqar.17a@gmail.com>
+ */
+namespace KFuzzyMatcher
+{
+/**
+ * @brief simple fuzzy matching of chars in @p pattern with chars in @p str
+ * sequentially. If there is a match, it will return true and false otherwise.
+ * There is no scoring. You should use this if score is not important for you
+ * and only matching is important.
+ *
+ * @param pattern to search for. For e.g., text entered by a user to filter a
+ * list
+ * @param str the current string from your list of strings
+ */
+KCOREADDONS_EXPORT bool matchSimple(const QStringView pattern, const QStringView str);
+
+/**
+ * @brief This should be the main function to use most of the times. @p outscore
+ * is the score of this match and should be used to sort the results later.
+ * Without sorting of the results this function won't very effective. Also note
+ * that this function scores separator matches higher than sequential matches.
+ * See @ref matchSequential for details
+ *
+ * If you are using this function in a QSortFilterProxyModel based class, keep
+ * in mind that during the initial load of the model when the pattern is empty,
+ * this function will return false and as a result your "view" will be empty. To
+ * prevent this, you should place a check in the beginning of your function
+ * which returns true if the pattern is empty.
+ *
+ * @param pattern to search for. For e.g., text entered by a user to filter a
+ * list
+ * @param str the current string from your list of strings
+ * @param outScore The output score of a particular match. This determines the
+ * quality of match and should be used for sorting later.
+ * @return true on a successful match.
+ */
+KCOREADDONS_EXPORT bool match(const QStringView pattern, const QStringView str, int &outScore);
+
+/**
+ * @brief This is a special case function which doesn't score separator matches
+ * higher than sequential matches. For example, if you have the following
+ * strings: Sort Items Split or join a tag and you use @ref match, then it can
+ * be that the second result is scored better than the first one despite the
+ * fact that the first one is an exact match. This happens because @ref match
+ * will score separator matches higher than sequential matches. Use this
+ * function if you want sequential matches to take preference.
+ *
+ * @param pattern to search for. For e.g., text entered by a user to filter a
+ * list
+ * @param str the current string from your list of strings
+ * @param outScore The output score of a particular match. This determines the
+ * quality of match and should be used for sorting later.
+ * @return true on a successful match.
+ */
+KCOREADDONS_EXPORT bool matchSequential(const QStringView pattern,
+                                        const QStringView str,
+                                        int &outScore);
+
+/**
+ * @brief This is a utility function to display what is being matched.
+ *
+ * Example:
+ * \code
+ * pattern = "kate", str = "kateapp" and @p htmlTag = "<b>"
+ * output =  <b>k</b><b>a</b><b>t</b><b>e</b>app
+ *
+ * which will be visible to user as <b>kate</b>app.
+ * \endcode
+ *
+ * @param pattern is the current pattern entered by user
+ * @param str is the string that will be wrapped with @p htmlTag. String will be
+ * modified
+ * @param htmlTag is the html tag you want to use for example <b> or <span
+ * style=...>
+ * @param htmlTagClose is the corresponding closing tag for @p htmlTag. The
+ * function does not check that whether it is a closing tag for @p htmlTag or
+ * not.
+ * @return htmlTag wrapped output string. It is the same as @p str
+ */
+KCOREADDONS_EXPORT QString toFuzzyMatchedDisplayString(const QStringView pattern,
+                                                       QString &str,
+                                                       const QString &htmlTag,
+                                                       const QString &htmlTagClose);
+
+} // namespace KFuzzyMatcher
