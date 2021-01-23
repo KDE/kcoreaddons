229e7c82618b387fe464cd89bffb03d03f867d3a
Waqar Ahmed <waqar.17a@gmail.com>
2021-01-23 14:21:27 +0500

    Add license headers to fuzzy-matcher test files

--- autotests/kfuzzymatchertest.cpp
+++ autotests/kfuzzymatchertest.cpp
@@ -1,3 +1,10 @@
+/*
+    This file is part of the KDE libraries
+
+    SPDX-FileCopyrightText: 2021 Waqar Ahmed <waqar.17a@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 #include "kfuzzymatchertest.h"
 
 #include <QString>
