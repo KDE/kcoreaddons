515907539bb11a638c64ceeb7a9eefff8549a090
Waqar Ahmed <waqar.17a@gmail.com>
2021-02-01 20:50:10 +0500

    Add example in doc and improve docs for fuzzy matcher

--- src/lib/text/kfuzzymatcher.h
+++ src/lib/text/kfuzzymatcher.h
@@ -12,18 +12,58 @@ class QString;
 class QStringView;
 
 /**
- * This namespace contains functions for fuzzy matching of strings.
- *
  * This code is ported to Qt from lib_fts:
  * https://github.com/forrestthewoods/lib_fts
  * which tries to replicate SublimeText like fuzzy matching.
  *
- * In general match() and matchSequential() will suffice for most usecases
+ * @note
+ * In general @ref match() and @ref matchSequential() will suffice for most usecases
  * but should the need arise to extend this for some particular reason, for example
  * scoring based on a different kind of separator, it should ideally be handled in a
  * separate internal method with an interface similar to the existing ones.
  *
  * All methods in here are stateless i.e., the input string will not be modified.
+ *
+ * If you are using it with @c QSortFilterProxyModel, and you choose to use @ref match()
+ * or @ref matchSequential, you need override both @c QSortFilterProxyModel::lessThan and
+ * @c QSortFilterProxyModel::filterAcceptsRow. A simple example:
+ *
+ * \code
+    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override
+    {
+        if (m_pattern.isEmpty()) {
+            return true;
+        }
+
+        int score = 0;
+        const auto idx = sourceModel()->index(sourceRow, 0, sourceParent);
+        const auto actionName = idx.data().toString().splitRef(QLatin1Char(':')).at(1);
+        const bool res = kfts::fuzzy_match_sequential(m_pattern, actionName, score);
+        // store the score in the source model
+        sourceModel()->setData(idx, score, ScoreRole);
+        return res;
+    }
+
+    bool lessThan(const QModelIndex &sourceLeft, const QModelIndex &sourceRight) const override
+    {
+        // use the score here to sort
+        const int l = sourceLeft.data(ScoreRole).toInt();
+        const int r = sourceRight.data(ScoreRole).toInt();
+        return l < r;
+    }
+ * \endcode
+ *
+ * Additionally you must not use @c invalidateFilter() if you go with the above approach. Instead
+ * use @c beginResetModel()/@c endResetModel():
+ *
+ * \code
+ *  Q_SLOT void setFilterString(const QString &string)
+    {
+        beginResetModel();
+        m_pattern = string;
+        endResetModel();
+    }
+ * \endcode
  *
  * @short Namespace for fuzzy matching of strings
  * @author Waqar Ahmed <waqar.17a@gmail.com>
@@ -32,18 +72,21 @@ namespace KFuzzyMatcher
 {
 
 /**
- * @brief The Result struct
+ * @brief The result of a fuzzy match
  */
 struct KCOREADDONS_EXPORT Result
 {
-    /** Score of this match */
+    /** Score of this match. This can be negative. if @ref matched is @c false
+        then the score will be useless for usual cases because the string will
+        get filtered out.
+    */
     int score;
-    /** match succesful? */
+    /** @c true if match was succesful */
     bool matched;
 };
 
 /**
- * @brief simple fuzzy matching of chars in @p pattern with chars in @p str
+ * @brief Simple fuzzy matching of chars in @p pattern with chars in @p str
  * sequentially. If there is a match, it will return true and false otherwise.
  * There is no scoring. You should use this if score is not important for you
  * and only matching is important.
@@ -51,7 +94,7 @@ struct KCOREADDONS_EXPORT Result
  * @param pattern to search for. For e.g., text entered by a user to filter a
  * list
  * @param str the current string from your list of strings
- * @return true on sucessful match
+ * @return @c true on sucessful match
  *
  * @since 5.79
  */
@@ -59,26 +102,26 @@ KCOREADDONS_EXPORT bool matchSimple(QStringView pattern, QStringView str);
 
 /**
  * @brief This should be the function to use if the strings you are matching
- * consists of file names, code etc. @p outscore is the score of this match and
- * should be used to sort the results later. Without sorting of the results this
- * function won't very effective. Also note that this function scores separator
- * matches higher than sequential matches.
+ * consist of file names, code etc.
+ *
+ * The return value of this function contains @ref Result#score which should be used to
+ * sort the results. Without sorting of the results this function won't very effective.
+ * Also note that this function scores separator matches higher than sequential matches.
  * See @ref matchSequential for more details
  *
- * If you are using this function in a QSortFilterProxyModel based class, keep
+ * If you are using this function in a @c QSortFilterProxyModel based class, keep
  * in mind that during the initial load of the model when the pattern is empty,
  * this function will return false and as a result your "view" will be empty. To
  * prevent this, you should place a check in the beginning of your function
  * which returns true if the pattern is empty.
  *
  * @param pattern to search for. For e.g., text entered by a user to filter a
- * list
+ * list or model
  * @param str the current string from your list of strings
- * @param outScore The output score of a particular match. This determines the
- * quality of match and should be used for sorting later.
- * @return A @ref Result type with score of this match and whether there is a match.
- * If there is no match, score is likely useless as the string will be filtered out.
- * If the match is successful, score must be used to sort the results.
+ * @return A @ref Result type with score of this match and whether the match was
+ * successful. If there is no match, score is likely useless as the string will
+ * be filtered out. If the match is successful, score must be used to sort the
+ * results.
  *
  * @since 5.79
  */
@@ -94,20 +137,20 @@ KCOREADDONS_EXPORT Result match(QStringView pattern, QStringView str);
  * "Split or join a tag"
  * \endcode
  *
- * and you use @ref match, then it can be that the second result is scored
- * better than the first one despite the fact that the first one is an exact
- * match. This happens because @ref match will score separator matches higher
- * than sequential matches. Use this function if you want sequential matches
- * to take preference. This will be needed if you are filtering user interface
- * strings.
- * to take preference.
+ * and you use @ref match with pattern @c "sort", then it can be that the second
+ * result is scored better than the first one despite the fact that the first one
+ * is an exact match. This happens because @ref match will score separator matches
+ * higher than sequential matches. Use this function if you want sequential matches
+ * to take preference. This can be useful if you are filtering user interface
+ * strings
  *
  * @param pattern to search for. For e.g., text entered by a user to filter a
- * list
+ * list or model
  * @param str the current string from your list of strings
- * @return A @ref Result type with score of this match and whether there is a match.
- * If there is no match, score is likely useless as the string will be filtered out.
- * If the match is successful, score must be used to sort the results.
+ * @return A @ref Result type with score of this match and whether the match was
+ * successful. If there is no match, score is likely useless as the string will
+ * be filtered out. If the match is successful, score must be used to sort the
+ * results.
  *
  * @since 5.79
  */
@@ -115,23 +158,23 @@ KCOREADDONS_EXPORT Result matchSequential(QStringView pattern, QStringView str);
 
 /**
  * @brief This is a utility function to display what is being matched.
+ * You can use the output of this function with a @c QTextDocument
+ * or in a @c QStyledItemDelegate.
  *
  * Example:
  * \code
- * pattern = "kate", str = "kateapp" and @p htmlTag = "<b>"
- * output =  <b>k</b><b>a</b><b>t</b><b>e</b>app
- *
- * which will be visible to user as <b>kate</b>app.
+ * pattern = "kate", str = "kateapp", htmlTag = "<b>"
+ * output =  <b>kate</b>app
  * \endcode
  *
  * @param pattern is the current pattern entered by user
  * @param str is the string that will be wrapped with @p htmlTag.
- * @param htmlTag is the html tag you want to use for example <b> or <span
- * style=...>
+ * @param htmlTag is the html tag you want to use for example \<b\> or \<span
+ * style=...\>
  * @param htmlTagClose is the corresponding closing tag for @p htmlTag. The
  * function does not check that whether it is a closing tag for @p htmlTag or
  * not.
- * @return htmlTag wrapped output string. It is the same as @p str
+ * @return html styled output string
  *
  * @since 5.79
  */
