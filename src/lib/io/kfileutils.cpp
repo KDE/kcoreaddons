Sonic-DE/sonic-frameworks-core-addons

Prevent QSet double-lookups
That happens when QSet::contains() is used, followed by QSet::insert().

Instead use a std::set.

NO_CHANGELOG

src/lib/io/kfileutils.cpp
@@ -13,7 +13,8 @@
 #include <QFileInfo>
 #include <QMimeDatabase>
 #include <QRegularExpression>
-#include <QSet>
+
+#include <set>
 
 QString KFileUtils::makeSuggestedName(const QString &oldName)
 {
@@ -72,14 +73,14 @@ QString KFileUtils::suggestName(const QUrl &baseURL, const QString &oldName)
 QStringList KFileUtils::findAllUniqueFiles(const QStringList &dirs, const QStringList &nameFilters)
 {
     QStringList foundFilePaths;
-    QSet<QString> foundFileNames;
+    std::set<QString> foundFileNames;
     for (const QString &dir : dirs) {
         QDirIterator it(dir, nameFilters, QDir::Files);
         while (it.hasNext()) {
             it.next();
-            if (!foundFileNames.contains(it.fileName())) {
+            const auto [it, isFirstSeen] = foundFileNames.insert(it.fileName());
+            if (isFirstSeen) {
                 foundFilePaths << it.filePath();
-                foundFileNames << it.fileName();
             }
         }
     }

KDE/kcoreaddons

src/lib/io/kfileutils.cpp
@@ -13,7 +13,8 @@
 #include <QFileInfo>
 #include <QMimeDatabase>
 #include <QRegularExpression>
-#include <QSet>
+
+#include <set>
 
 QString KFileUtils::makeSuggestedName(const QString &oldName)
 {
@@ -72,14 +73,14 @@ QString KFileUtils::suggestName(const QUrl &baseURL, const QString &oldName)
 QStringList KFileUtils::findAllUniqueFiles(const QStringList &dirs, const QStringList &nameFilters)
 {
     QStringList foundFilePaths;
-    QSet<QString> foundFileNames;
+    std::set<QString> foundFileNames;
     for (const QString &dir : dirs) {
         QDirIterator it(dir, nameFilters, QDir::Files);
         while (it.hasNext()) {
             it.next();
-            if (!foundFileNames.contains(it.fileName())) {
+            const auto [iter, isFirstSeen] = foundFileNames.insert(it.fileName());
+            if (isFirstSeen) {
                 foundFilePaths << it.filePath();
-                foundFileNames << it.fileName();
             }
         }
     }