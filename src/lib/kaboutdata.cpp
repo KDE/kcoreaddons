Sonic-DE/sonic-frameworks-core-addons

Move utility method to read translated json values to dedicated header
These methods are not directly related to KPluginMetaData and can
be useful outside of it.

This way KPluginMetaData only contains the logic to read properties and
to list plugins from a namespace.

Task: https://phabricator.kde.org/T14787

src/lib/kaboutdata.cpp
@@ -12,6 +12,7 @@
 */
 
 #include "kaboutdata.h"
+#include "kjsonutils.h"
 #include "kpluginmetadata.h"
 
 #include <QCommandLineOption>
@@ -93,8 +94,8 @@ KAboutPerson &KAboutPerson::operator=(const KAboutPerson &other) = default;
 
 KAboutPerson KAboutPerson::fromJSON(const QJsonObject &obj)
 {
-    const QString name = KPluginMetaData::readTranslatedString(obj, QStringLiteral("Name"));
-    const QString task = KPluginMetaData::readTranslatedString(obj, QStringLiteral("Task"));
+    const QString name = KJSONUtils::readTranslatedString(obj, QStringLiteral("Name"));
+    const QString task = KJSONUtils::readTranslatedString(obj, QStringLiteral("Task"));
     const QString email = obj[QStringLiteral("Email")].toString();
     const QString website = obj[QStringLiteral("Website")].toString();
     const QString userName = obj[QStringLiteral("UserName")].toString();

src/lib/plugin/kpluginmetadata.cpp
@@ -11,6 +11,7 @@
 #include "desktopfileparser_p.h"
 
 #include "kcoreaddons_debug.h"
+#include "kjsonutils.h"
 #include <QCoreApplication>
 #include <QDir>
 #include <QDirIterator>
@@ -289,32 +290,17 @@ QStringList KPluginMetaData::readStringList(const QJsonObject &obj, const QStrin
         return QStringList(asString);
     }
 }
-#endif
 
 QJsonValue KPluginMetaData::readTranslatedValue(const QJsonObject &jo, const QString &key, const QJsonValue &defaultValue)
 {
-    QString languageWithCountry = QLocale().name();
-    auto it = jo.constFind(key + QLatin1Char('[') + languageWithCountry + QLatin1Char(']'));
-    if (it != jo.constEnd()) {
-        return it.value();
-    }
-    const QStringView language = QStringView(languageWithCountry).mid(0, languageWithCountry.indexOf(QLatin1Char('_')));
-    it = jo.constFind(key + QLatin1Char('[') + language + QLatin1Char(']'));
-    if (it != jo.constEnd()) {
-        return it.value();
-    }
-    // no translated value found -> check key
-    it = jo.constFind(key);
-    if (it != jo.constEnd()) {
-        return jo.value(key);
-    }
-    return defaultValue;
+    return KJSONUtils::readTranslatedValue(jo, key, defaultValue);
 }
 
 QString KPluginMetaData::readTranslatedString(const QJsonObject &jo, const QString &key, const QString &defaultValue)
 {
     return readTranslatedValue(jo, key, defaultValue).toString(defaultValue);
 }
+#endif
 
 static inline void addPersonFromJson(const QJsonObject &obj, QList<KAboutPerson> *out)
 {
@@ -365,7 +351,7 @@ QString KPluginMetaData::category() const
 
 QString KPluginMetaData::description() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Description"));
+    return KJSONUtils::readTranslatedString(rootObject(), QStringLiteral("Description"));
 }
 
 QString KPluginMetaData::iconName() const
@@ -385,18 +371,18 @@ QString KPluginMetaData::licenseText() const
 
 QString KPluginMetaData::name() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Name"));
+    return KJSONUtils::readTranslatedString(rootObject(), QStringLiteral("Name"));
 }
 
 QString KPluginMetaData::copyrightText() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Copyright"));
+    return KJSONUtils::readTranslatedString(rootObject(), QStringLiteral("Copyright"));
 }
 
 #if KCOREADDONS_BUILD_DEPRECATED_SINCE(5, 87)
 QString KPluginMetaData::extraInformation() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("ExtraInformation"));
+    return KJSONUtils::readTranslatedString(rootObject(), QStringLiteral("ExtraInformation"));
 }
 #endif
 

src/lib/plugin/kpluginmetadata.h
@@ -487,21 +487,29 @@ public:
     static QStringList readStringList(const QJsonObject &jo, const QString &key);
 #endif
 
+#if KCOREADDONS_ENABLE_DEPRECATED_SINCE(5, 88)
     /**
      * Reads a value from @p jo but unlike QJsonObject::value() it allows different entries for each locale
      * This is done by appending the locale identifier in brackets to the key (e.g. "[de_DE]" or "[es]")
      * When looking for a key "foo" with German (Germany) locale we will first attempt to read "foo[de_DE]",
      * if that does not exist "foo[de]", finally falling back to "foo" if that also doesn't exist.
      * @return the translated value for @p key from @p jo or @p defaultValue if @p key was not found
+     * @deprecated Since 5.88, use KJSONUtils::readTranslatedValue instead
      */
+    KCOREADDONS_DEPRECATED_VERSION(5, 88, "Use KJSONUtils::readTranslatedValue instead")
     static QJsonValue readTranslatedValue(const QJsonObject &jo, const QString &key, const QJsonValue &defaultValue = QJsonValue());
+#endif
 
+#if KCOREADDONS_ENABLE_DEPRECATED_SINCE(5, 88)
     /**
      * @return the translated value of @p key from @p jo as a string or @p defaultValue if @p key was not found
      * or the value for @p key is not of type string
      * @see KPluginMetaData::readTranslatedValue(const QJsonObject &jo, const QString &key)
+     * @deprecated Since 5.88, use KJSONUtils::readTranslatedString instead
      */
+    KCOREADDONS_DEPRECATED_VERSION(5, 88, "Use KJSONUtils::readTranslatedString instead")
     static QString readTranslatedString(const QJsonObject &jo, const QString &key, const QString &defaultValue = QString());
+#endif
 
     /**
      * @return @c true if this object is equal to @p other, otherwise @c false

KDE/kcoreaddons

src/lib/kaboutdata.cpp
@@ -12,6 +12,7 @@
 */
 
 #include "kaboutdata.h"
+#include "kjsonutils.h"
 #include "kpluginmetadata.h"
 
 #include <QCommandLineOption>
@@ -93,8 +94,8 @@ KAboutPerson &KAboutPerson::operator=(const KAboutPerson &other) = default;
 
 KAboutPerson KAboutPerson::fromJSON(const QJsonObject &obj)
 {
-    const QString name = KPluginMetaData::readTranslatedString(obj, QStringLiteral("Name"));
-    const QString task = KPluginMetaData::readTranslatedString(obj, QStringLiteral("Task"));
+    const QString name = KJsonUtils::readTranslatedString(obj, QStringLiteral("Name"));
+    const QString task = KJsonUtils::readTranslatedString(obj, QStringLiteral("Task"));
     const QString email = obj[QStringLiteral("Email")].toString();
     const QString website = obj[QStringLiteral("Website")].toString();
     const QString userName = obj[QStringLiteral("UserName")].toString();

src/lib/plugin/kpluginmetadata.cpp
@@ -11,6 +11,7 @@
 #include "desktopfileparser_p.h"
 
 #include "kcoreaddons_debug.h"
+#include "kjsonutils.h"
 #include <QCoreApplication>
 #include <QDir>
 #include <QDirIterator>
@@ -289,32 +290,17 @@ QStringList KPluginMetaData::readStringList(const QJsonObject &obj, const QStrin
         return QStringList(asString);
     }
 }
-#endif
 
 QJsonValue KPluginMetaData::readTranslatedValue(const QJsonObject &jo, const QString &key, const QJsonValue &defaultValue)
 {
-    QString languageWithCountry = QLocale().name();
-    auto it = jo.constFind(key + QLatin1Char('[') + languageWithCountry + QLatin1Char(']'));
-    if (it != jo.constEnd()) {
-        return it.value();
-    }
-    const QStringView language = QStringView(languageWithCountry).mid(0, languageWithCountry.indexOf(QLatin1Char('_')));
-    it = jo.constFind(key + QLatin1Char('[') + language + QLatin1Char(']'));
-    if (it != jo.constEnd()) {
-        return it.value();
-    }
-    // no translated value found -> check key
-    it = jo.constFind(key);
-    if (it != jo.constEnd()) {
-        return jo.value(key);
-    }
-    return defaultValue;
+    return KJsonUtils::readTranslatedValue(jo, key, defaultValue);
 }
 
 QString KPluginMetaData::readTranslatedString(const QJsonObject &jo, const QString &key, const QString &defaultValue)
 {
     return readTranslatedValue(jo, key, defaultValue).toString(defaultValue);
 }
+#endif
 
 static inline void addPersonFromJson(const QJsonObject &obj, QList<KAboutPerson> *out)
 {
@@ -365,7 +351,7 @@ QString KPluginMetaData::category() const
 
 QString KPluginMetaData::description() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Description"));
+    return KJsonUtils::readTranslatedString(rootObject(), QStringLiteral("Description"));
 }
 
 QString KPluginMetaData::iconName() const
@@ -385,18 +371,18 @@ QString KPluginMetaData::licenseText() const
 
 QString KPluginMetaData::name() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Name"));
+    return KJsonUtils::readTranslatedString(rootObject(), QStringLiteral("Name"));
 }
 
 QString KPluginMetaData::copyrightText() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("Copyright"));
+    return KJsonUtils::readTranslatedString(rootObject(), QStringLiteral("Copyright"));
 }
 
 #if KCOREADDONS_BUILD_DEPRECATED_SINCE(5, 87)
 QString KPluginMetaData::extraInformation() const
 {
-    return readTranslatedString(rootObject(), QStringLiteral("ExtraInformation"));
+    return KJsonUtils::readTranslatedString(rootObject(), QStringLiteral("ExtraInformation"));
 }
 #endif
 

src/lib/plugin/kpluginmetadata.h
@@ -487,21 +487,29 @@ public:
     static QStringList readStringList(const QJsonObject &jo, const QString &key);
 #endif
 
+#if KCOREADDONS_ENABLE_DEPRECATED_SINCE(5, 88)
     /**
      * Reads a value from @p jo but unlike QJsonObject::value() it allows different entries for each locale
      * This is done by appending the locale identifier in brackets to the key (e.g. "[de_DE]" or "[es]")
      * When looking for a key "foo" with German (Germany) locale we will first attempt to read "foo[de_DE]",
      * if that does not exist "foo[de]", finally falling back to "foo" if that also doesn't exist.
      * @return the translated value for @p key from @p jo or @p defaultValue if @p key was not found
+     * @deprecated Since 5.88, use KJsonUtils::readTranslatedValue instead
      */
+    KCOREADDONS_DEPRECATED_VERSION(5, 88, "Use KJsonUtils::readTranslatedValue instead")
     static QJsonValue readTranslatedValue(const QJsonObject &jo, const QString &key, const QJsonValue &defaultValue = QJsonValue());
+#endif
 
+#if KCOREADDONS_ENABLE_DEPRECATED_SINCE(5, 88)
     /**
      * @return the translated value of @p key from @p jo as a string or @p defaultValue if @p key was not found
      * or the value for @p key is not of type string
      * @see KPluginMetaData::readTranslatedValue(const QJsonObject &jo, const QString &key)
+     * @deprecated Since 5.88, use KJsonUtils::readTranslatedString instead
      */
+    KCOREADDONS_DEPRECATED_VERSION(5, 88, "Use KJsonUtils::readTranslatedString instead")
     static QString readTranslatedString(const QJsonObject &jo, const QString &key, const QString &defaultValue = QString());
+#endif
 
     /**
      * @return @c true if this object is equal to @p other, otherwise @c false