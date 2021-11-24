Sonic-DE/sonic-frameworks-core-addons

Allow KPluginMetaData to represent static plugins
First step towards supporting static plugins by only having one
code path.

Task: https://phabricator.kde.org/T14727

src/lib/plugin/kpluginfactory.cpp
@@ -33,8 +33,13 @@ KPluginFactory::~KPluginFactory() = default;
 KPluginFactory::Result<KPluginFactory> KPluginFactory::loadFactory(const KPluginMetaData &data)
 {
     Result<KPluginFactory> result;
-    QPluginLoader loader(data.fileName());
-    QObject *obj = loader.instance();
+    QObject *obj = nullptr;
+    if (data.isStaticPlugin()) {
+        obj = data.getStaticPlugin().instance();
+    } else {
+        QPluginLoader loader(data.fileName());
+        obj = loader.instance();
+    }
 
     if (!obj) {
         result.errorString = tr("Could not load plugin from %1").arg(data.fileName());

src/lib/plugin/kpluginmetadata.cpp
@@ -29,6 +29,7 @@ class KPluginMetaDataPrivate : public QSharedData
 {
 public:
     QString metaDataFileName;
+    std::optional<QStaticPlugin> staticPlugin = std::nullopt;
     static void forEachPlugin(const QString &directory, std::function<void(const QString &)> callback)
     {
         QStringList dirsToCheck;
@@ -154,6 +155,12 @@ KPluginMetaData::KPluginMetaData(const QJsonObject &metaData, const QString &plu
         d = new KPluginMetaDataPrivate;
         d->metaDataFileName = metaDataFile;
     }
+}
+
+KPluginMetaData::KPluginMetaData(QStaticPlugin plugin, const QJsonObject &metaData)
+{
+    d->staticPlugin = plugin;
+    m_metaData = plugin.metaData().isEmpty() ? metaData : plugin.metaData();
 }
 
 KPluginMetaData KPluginMetaData::findPluginById(const QString &directory, const QString &pluginId)
@@ -605,4 +612,14 @@ QVariantList KPluginMetaData::translatorsVariant() const
 QVariantList KPluginMetaData::otherContributorsVariant() const
 {
     return listToVariant(otherContributors());
+}
+bool KPluginMetaData::isStaticPlugin() const
+{
+    return d && d->staticPlugin.has_value();
+}
+
+QStaticPlugin KPluginMetaData::getStaticPlugin() const
+{
+    Q_ASSERT(d->staticPlugin.has_value());
+    return d->staticPlugin.value();
 }

KDE/kcoreaddons

src/lib/plugin/kpluginfactory.cpp
@@ -33,8 +33,13 @@ KPluginFactory::~KPluginFactory() = default;
 KPluginFactory::Result<KPluginFactory> KPluginFactory::loadFactory(const KPluginMetaData &data)
 {
     Result<KPluginFactory> result;
-    QPluginLoader loader(data.fileName());
-    QObject *obj = loader.instance();
+    QObject *obj = nullptr;
+    if (data.isStaticPlugin()) {
+        obj = data.staticPlugin().instance();
+    } else {
+        QPluginLoader loader(data.fileName());
+        obj = loader.instance();
+    }
 
     if (!obj) {
         result.errorString = tr("Could not load plugin from %1").arg(data.fileName());

src/lib/plugin/kpluginmetadata.cpp
@@ -26,10 +26,13 @@
 #include "kpluginfactory.h"
 #include "kpluginloader.h"
 
+#include <optional>
+
 class KPluginMetaDataPrivate : public QSharedData
 {
 public:
     QString metaDataFileName;
+    std::optional<QStaticPlugin> staticPlugin = std::nullopt;
     static void forEachPlugin(const QString &directory, std::function<void(const QString &)> callback)
     {
         QStringList dirsToCheck;
@@ -155,6 +158,12 @@ KPluginMetaData::KPluginMetaData(const QJsonObject &metaData, const QString &plu
         d = new KPluginMetaDataPrivate;
         d->metaDataFileName = metaDataFile;
     }
+}
+
+KPluginMetaData::KPluginMetaData(QStaticPlugin plugin, const QJsonObject &metaData)
+{
+    d->staticPlugin = plugin;
+    m_metaData = plugin.metaData().isEmpty() ? metaData : plugin.metaData();
 }
 
 KPluginMetaData KPluginMetaData::findPluginById(const QString &directory, const QString &pluginId)
@@ -593,4 +602,14 @@ QVariantList KPluginMetaData::translatorsVariant() const
 QVariantList KPluginMetaData::otherContributorsVariant() const
 {
     return listToVariant(otherContributors());
+}
+bool KPluginMetaData::isStaticPlugin() const
+{
+    return d && d->staticPlugin.has_value();
+}
+
+QStaticPlugin KPluginMetaData::staticPlugin() const
+{
+    Q_ASSERT(d->staticPlugin.has_value());
+    return d->staticPlugin.value();
 }