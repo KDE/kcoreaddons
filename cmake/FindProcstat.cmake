b6f5a336ac52ed18715bfe58803a7a975a892f69
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-12-28 13:03:12 +0100

    FindProcstat: Fix CMake warning and use an imported target
    Previously we were getting the following warning:
    ```
    The package name passed to `find_package_handle_standard_args` (PROCSTAT)
    does not match the name of the calling package (Procstat).  This can lead
    to problems in calling code that expects `find_package` result variables
    (e.g., `_FOUND`) to follow a certain pattern.
    ```

--- cmake/FindProcstat.cmake
+++ cmake/FindProcstat.cmake
@@ -1,25 +1,52 @@
-# - Try to find procstat library
-# Once done this will define
-#
-#  PROCSTAT_FOUND - system has procstat
-#  PROCSTAT_INCLUDE_DIR - the procstat include directory
-#  PROCSTAT_LIBRARIES - The libraries needed to use procstat
-
 # SPDX-FileCopyrightText: 2019 Tobias C. Berner <tcberner@FreeBSD.org>
 #
 # SPDX-License-Identifier: BSD-2-Clause
+#[=======================================================================[.rst:
+FindProcstat
+-------
+
+Finds the procstat library.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``Procstat::Procstat``
+  The procstat library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``Procstat_FOUND``
+  True if the system has the libprocstat library.
+``Procstat_INCLUDE_DIRS``
+  Include directories needed to use procstat.
+``Procstat_LIBRARIES``
+  Libraries needed to link to libprocstat.
+
+#]=======================================================================]
 
 
-FIND_PATH(PROCSTAT_INCLUDE_DIR libprocstat.h)
+find_path(Procstat_INCLUDE_DIRS libprocstat.h)
 
-FIND_LIBRARY(PROCSTAT_LIBRARIES NAMES procstat )
+find_library(Procstat_LIBRARIES NAMES procstat)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PROCSTAT DEFAULT_MSG PROCSTAT_INCLUDE_DIR PROCSTAT_LIBRARIES )
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Procstat DEFAULT_MSG Procstat_INCLUDE_DIRS Procstat_LIBRARIES)
 
-set_package_properties(PROCSTAT PROPERTIES
+set_package_properties(Procstat PROPERTIES
     URL "https://github.com/freebsd/freebsd/tree/master/lib/libprocstat"
     DESCRIPTION "Library to access process information"
 )
+mark_as_advanced(Procstat_INCLUDE_DIRS Procstat_LIBRARIES)
+if(Procstat_FOUND AND NOT TARGET Procstat::Procstat)
+    add_library(Procstat::Procstat UNKNOWN IMPORTED)
+    set_target_properties(Procstat::Procstat PROPERTIES
+        IMPORTED_LOCATION "${Procstat_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Procstat_INCLUDE_DIRS}"
+    )
+endif()
 
-MARK_AS_ADVANCED(PROCSTAT_INCLUDE_DIR PROCSTAT_LIBRARIES)
