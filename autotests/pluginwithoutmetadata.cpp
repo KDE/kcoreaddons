ec1b3d2d484655ffc6c00abd1fe15e88f253042a
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-12-29 13:49:06 +0100

    GIT_SILENT Add missing license header for plugin

--- autotests/pluginwithoutmetadata.cpp
+++ autotests/pluginwithoutmetadata.cpp
@@ -1,3 +1,8 @@
+/*
+    SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
 #include <kpluginfactory.h>
 
 class PluginWithoutMetaData : public QObject
