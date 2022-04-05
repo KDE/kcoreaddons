45a2e5cf3a47313888654cfd00d1a4d9e0e24864
Adriaan de Groot <groot@kde.org>
2022-04-05 13:36:00 +0200

    autotests: check for ZFS and expect failure then

--- autotests/klistopenfilesjobtest_unix.cpp
+++ autotests/klistopenfilesjobtest_unix.cpp
@@ -16,7 +16,7 @@
 
 #ifdef Q_OS_FREEBSD
 // See implementation note in testOpenFiles()
-#include <QLocalServer>
+#include <QProcess>
 #endif
 
 QTEST_MAIN(KListOpenFilesJobTest)
@@ -46,21 +46,38 @@ void KListOpenFilesJobTest::testOpenFiles()
     QTemporaryDir tempDir;
     QFile tempFile(tempDir.path() + QStringLiteral("/file"));
     QVERIFY(tempFile.open(QIODevice::WriteOnly));
+    bool xfail_zfs = false;  // Expected failure because of ZFS
 #ifdef Q_OS_FREEBSD
     // FIXME: On FreeBSD, lsof does not support zfs (as of 2022), see
     // https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=253553
     //
-    // This affects regular files only. So for FreeBSD, create
-    // a named socket as well: this **does** show up in lsof.
-    QLocalServer namedSocket;
-    const QString namedPath = tempDir.path() + QStringLiteral("/socket");
-    QVERIFY(namedSocket.listen(namedPath));
+    // This affects regular files only. So for FreeBSD, check if
+    // the tempDir seems to be on a ZFS filesystem, e.g.
+    //
+    // ```
+    // [adridg@beastie .../invent/kcoreaddons]$ lsof +d /tmp > /dev/null
+    // lsof: WARNING: no ZFS support has been defined.
+    //   See 00FAQ for more information.
+    // ```
+    {
+        QProcess lsof;
+        lsof.start(QStringLiteral("lsof"), {QStringLiteral("+d"), tempDir.path()});
+        lsof.waitForFinished();
+
+        auto stderr = lsof.readAllStandardError();
+        xfail_zfs = (lsof.exitCode() != 0) && stderr.contains("ZFS");
+    }
 #endif
 
     auto job = new KListOpenFilesJob(tempDir.path());
     QVERIFY2(job->exec(), qPrintable(job->errorString()));
     QCOMPARE(job->error(), KJob::NoError);
     auto processInfoList = job->processInfoList();
+    if (xfail_zfs) {
+        // Ths list is empty, so the subsequent find and validity-checks
+        // don't make sense.
+        QEXPECT_FAIL("", "lsof(8) does not support regular files on ZFS", Abort);
+    }
     QVERIFY(!processInfoList.empty());
     auto testProcessIterator = std::find_if(processInfoList.begin(), processInfoList.end(), [](const KProcessList::KProcessInfo &info) {
         return info.pid() == QCoreApplication::applicationPid();
