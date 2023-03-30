18137570bb596fe75ea5936eb4e9c1fb78dd9e81
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-03-30 07:05:36 +0200

    Add missing SPDX info to test files

--- autotests/plugin/plugins.cpp
+++ autotests/plugin/plugins.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Alexander Lohnau <alexander.lohnau@gmx.de>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
 #include "plugins.h"
 
 MyPlugin::MyPlugin(QObject *parent)
--- autotests/plugin/widgetsplugin.cpp
+++ autotests/plugin/widgetsplugin.cpp
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2023 Alexander Lohnau <alexander.lohnau@gmx.de>
+// SPDX-License-Identifier: LGPL-2.0-or-later
 #include <QWidget>
 #include <kpluginfactory.h>
 
