0bf89d6a42d272999af78f77aa7f7500f3cf9e79
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-06-16 16:39:17 +0200

    Include only needed headers instead of QtQml module header
    NO_CHANGELOG

--- src/qml/kcoreaddonsplugin.cpp
+++ src/qml/kcoreaddonsplugin.cpp
@@ -6,8 +6,8 @@
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
 
+#include <QQmlEngine>
 #include <QQmlExtensionPlugin>
-#include <QtQml>
 
 #include "formats.h"
 #include "kuserproxy.h"
