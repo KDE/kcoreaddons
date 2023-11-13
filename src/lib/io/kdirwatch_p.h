Sonic-DE/sonic-frameworks-core-addons

kdirwatch: expand ref counting system to keep account of public instances
this supplements the previous system where we would set the `d` of Entry
instances to nullptr. This notably didn't cover KDirWatch instances that
do not have any entries though (such as when ::self() is getting called
but never installs any watches).
to solve this problem we now do object based refcounting where the
public refs and unrefs `this` and we keep a record of currently active
references inside the private

src/lib/io/kdirwatch_p.h
@@ -186,8 +186,8 @@ public:
 
     static bool isNoisyFile(const char *filename);
 
-    void ref();
-    void unref();
+    void ref(KDirWatch *watch);
+    void unref(KDirWatch *watch);
 
 #if HAVE_SYS_INOTIFY_H
     QString inotifyEventName(const inotify_event *event) const;
@@ -234,6 +234,7 @@ public:
 private:
     // Public objects that reference this thread-local private instance.
     uint m_references;
+    QVector<KDirWatch *> m_referencesObjects;
 };
 
 QDebug operator<<(QDebug debug, const KDirWatchPrivate &dwp);

KDE/kcoreaddons

(cherry picked from commit bc909ba4f99279c3f652c3b38c51d4da0ac9e913)

src/lib/io/kdirwatch_p.h
@@ -194,8 +194,8 @@ public:
 
     static bool isNoisyFile(const char *filename);
 
-    void ref();
-    void unref();
+    void ref(KDirWatch *watch);
+    void unref(KDirWatch *watch);
 
 public Q_SLOTS:
     void slotRescan();
@@ -248,7 +248,7 @@ public:
 
 private:
     // Public objects that reference this thread-local private instance.
-    uint m_references;
+    QList<KDirWatch *> m_referencesObjects;
 };
 
 QDebug operator<<(QDebug debug, const KDirWatchPrivate::Entry &entry);