d66a1c0c8f0bd5fd2b1c2ec2e648545068fea7c1
Carl Schwan <carl@carlschwan.eu>
2024-01-02 14:17:47 +0100

    Move KSignalHandler to LGPL-2.1-or-later
    LGPL-2.0-only is not authorized by our license policy

--- src/lib/util/ksignalhandler.h
+++ src/lib/util/ksignalhandler.h
@@ -1,7 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2021 Aleix Pol Gonzalez <aleixpol@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-only
+    SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
 #ifndef KSIGNALHANDLER_H
