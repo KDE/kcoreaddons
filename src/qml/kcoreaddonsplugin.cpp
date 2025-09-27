afbf31068eab6cbe27beada457a7e2095fc3b615
Thiago Sueto <herzenschein@gmail.com>
2025-09-27 14:10:22 -0300

    Document org.kde.coreaddons.AboutData

--- src/qml/kcoreaddonsplugin.cpp
+++ src/qml/kcoreaddonsplugin.cpp
@@ -28,6 +28,36 @@ class KCoreAddonsPlugin : public QQmlExtensionPlugin
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
 
 public:
+    /*!
+     * \qmltype AboutData
+     * \inqmlmodule org.kde.coreaddons
+     * \brief This type exposes project metadata set with KAboutData from the C++ side.
+     *
+     * This singleton exposes the following read-only properties:
+     *
+     * \list
+     * \li KAboutData::displayName as \c AboutData.displayName
+     * \li KAboutData::productName as \c AboutData.productName
+     * \li KAboutData::componentName as \c AboutData.componentName
+     * \li KAboutData::programLogo as \c AboutData.programLogo
+     * \li KAboutData::shortDescription as \c AboutData.shortDescription
+     * \li KAboutData::homepage as \c AboutData.homepage
+     * \li KAboutData::bugAddress as \c AboutData.bugAddress
+     * \li KAboutData::version as \c AboutData.version
+     * \li KAboutData::otherText as \c AboutData.otherText
+     * \li KAboutData::authors as \c AboutData.authors
+     * \li KAboutData::credits as \c AboutData.credits
+     * \li KAboutData::translators as \c AboutData.translators
+     * \li KAboutData::components as \c AboutData.components
+     * \li KAboutData::licenses as \c AboutData.licenses
+     * \li KAboutData::copyrightStatement as \c AboutData.copyrightStatement
+     * \li KAboutData::desktopFileName as \c AboutData.desktopFileName
+     * \endlist
+     *
+     * The KAboutData metadata is expected to be set in C++ (or equivalent KAboutData bindings).
+     *
+     * \sa KAboutData
+     */
     void registerTypes(const char *uri) override
     {
         qmlRegisterSingletonType(uri, 1, 0, "AboutData", [](QQmlEngine *engine, QJSEngine *) -> QJSValue {
